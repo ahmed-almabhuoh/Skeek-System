d4f6b8b64950c0dd144b9b8a2d4f20b2b965f1de
Ahmad Almabhoh <az540546@gmail.com>
2022-05-21 12:17:24 +0300

    Livewire-Search-Sheek

--- resources/views/back-end/parent.blade.php
+++ resources/views/back-end/parent.blade.php
@@ -14,12 +14,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="{{ asset('checkSystem/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('sheekSystem/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('checkSystem/dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('sheekSystem/dist/css/adminlte.min.css') }}">
 
     <!-- Toastr -->
-    <link rel="stylesheet" href="{{ asset('checkSystem/plugins/toastr/toastr.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('sheekSystem/plugins/toastr/toastr.min.css') }}">
 
     @yield('styles')
 </head>
@@ -83,7 +83,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
-                <img src="{{ asset('checkSystem/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                <img src="{{ asset('sheekSystem/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Sheek System</span>
             </a>
@@ -93,7 +93,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="{{ asset('checkSystem/dist/img/user2-160x160.jpg') }}"
+                        <img src="{{ asset('sheekSystem/dist/img/user2-160x160.jpg') }}"
                             class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
@@ -212,11 +212,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- REQUIRED SCRIPTS -->
 
     <!-- jQuery -->
-    <script src="{{ asset('checkSystem/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('sheekSystem/plugins/jquery/jquery.min.js') }}"></script>
     <!-- Bootstrap 4 -->
-    <script src="{{ asset('checkSystem/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('sheekSystem/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- AdminLTE App -->
-    <script src="{{ asset('checkSystem/dist/js/adminlte.min.js') }}"></script>
+    <script src="{{ asset('sheekSystem/dist/js/adminlte.min.js') }}"></script>
 
 
     {{-- Sweet Alert --}}
@@ -224,7 +224,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     {{-- AXIOS LIBRARY --}}
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <!-- Toastr -->
-    <script src="{{ asset('checkSystem/plugins/toastr/toastr.min.js') }}"></script>
+    <script src="{{ asset('sheekSystem/plugins/toastr/toastr.min.js') }}"></script>
 
     @yield('scripts')
 </body>
