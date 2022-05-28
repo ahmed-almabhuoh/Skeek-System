a59606e1b0f9c9ca61afd6edcfb6b72fee256f7c
Ahmad Almabhoh <az540546@gmail.com>
2022-05-28 08:44:27 +0300

    Some-Edit-Admin-Dashboard-Recived-And-Paid-Sheeks

---
+++ resources/views/back-end/sheek/search-for-recived-sheek.blade.php
@@ -0,0 +1,62 @@
+@extends('back-end.index')
+
+@section('title', __('cms.sheeks'))
+@section('location', __('cms.sheeks'))
+@section('index', __('cms.index'))
+
+@section('styles')
+    @livewireStyles
+@endsection
+
+@section('content')
+    <livewire:search-sheek />
+@endsection
+
+@section('scripts')
+    @livewireScripts
+    <script>
+        function confirmDestroy(id, refrance) {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    destoy(id, refrance);
+                }
+            });
+        }
+
+        function destoy(id, refrance) {
+            // check-system/sheeks/{sheek} 
+            axios.delete('/check-system/sheeks/' + id)
+                .then(function(response) {
+                    // handle success
+                    console.log(response);
+                    refrance.closest('tr').remove();
+                    showDeletingMessage(response.data);
+                })
+                .catch(function(error) {
+                    // handle error
+                    console.log(error);
+                    showDeletingMessage(error.response.data);
+                })
+                .then(function() {
+                    // always executed
+                });
+        }
+
+        function showDeletingMessage(data) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Your work has been saved',
+                showConfirmButton: false,
+                timer: 2000
+            });
+        }
+    </script>
+@endsection
