62adcbe48de40a4622b17e088b0de2aabe8dba2c
Ahmad Almabhoh <az540546@gmail.com>
2022-07-17 11:12:54 +0300

    Create-User-Logs-Table

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Logs\UserLogsController;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    // Logs
+    public function storeUserLogs($action)
+    {
+        (new UserLogsController())->storeUserLog(request()->ip(), $action, Agent::device(), Agent::platform(), Agent::browser(), Agent::isDesktop(), Agent::isTablet(), Agent::isPhone(), Agent::isRobot(), Agent::version(Agent::browser()), Agent::version(Agent::platform()));
+    }
+    // -- Logs -- 
+
 }
---
+++ app/Http/Controllers/Logs/UserLogsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Logs;
+
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserLogsController extends Controller
+{
+    //
+
+    // Store Admin Logs
+    public function storeUserLog($ip, $action, $device = null, $os = null, $browser = null, $isDesktop = false, $isTablet = false, $isPhone = false, $isRobot = false, $browser_version = null, $platform_version = null)
+    {
+        DB::table('user_logs')->insert([
+            'ip' => $ip,
+            'action' => $action,
+            'device' => $device,
+            'os' => $os,
+            'browser' => $browser,
+            'isDesktop' => $isDesktop,
+            'isTablet' => $isTablet,
+            'isPhone' => $isPhone,
+            'isRobot' => $isRobot,
+            'browser_version' => $browser_version,
+            'platform_version' => $platform_version,
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'user_id' => auth('user')->user()->id,
+        ]);
+    }
+}
