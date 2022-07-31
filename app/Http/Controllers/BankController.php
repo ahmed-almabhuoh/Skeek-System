91f9c4a1a9453b515b23dd40f5145155dba7cbac
Ahmad Almabhoh <az540546@gmail.com>
2022-07-31 14:37:21 +0300

    Syncronouns-Checnges-Between-Static-And-Dynmaic

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -56,6 +56,9 @@ class BankController extends Controller
             ['active', '1'],
         ])->get();
 
+        $static_countries = DB::table('static_countries')->where('active', 1)->get();
+        $static_banks = DB::table('static_bank')->where('active', 1)->get();
+
         session([
             'created' => false,
             'title' => 'Failed',
@@ -63,6 +66,8 @@ class BankController extends Controller
         ]);
         return response()->view('back-end.banks.create', [
             'countries' => $countries,
+            'static_countries' => $static_countries,
+            'static_banks' => $static_banks,
         ]);
     }
 
@@ -74,38 +79,168 @@ class BankController extends Controller
      */
     public function store(CreateBankRequest $request)
     {
-        $bank = new Bank();
-        $bank->name = $request->input('name');
-        $bank->city = $request->input('city');
-        $bank->country_id = $request->input('country_id') ?? (Country::where('admin_id', auth('admin')->user()->id)->first())->id;
-        $bank->admin_id = auth('admin')->user()->id;
-        $isCreated = $bank->save();
-        if ($request->hasFile('sheek_image')) {
-            //abc.png
-            $sheekImageName = time() . '_sheek_images' . '.' . $request->file('sheek_image')->getClientOriginalExtension();
-            $request->file('sheek_image')->storePubliclyAs('public/img/', $sheekImageName);
-            DB::table('images')->insert([
-                'img' => $sheekImageName,
-                'bank_id' => $bank->id,
-            ]);
-        }
+        // Create new country manual
+        if ($request->input('static_country_id') == 0) {
+            $bank = new Bank();
+            $bank->name = $request->input('name');
+            $bank->city = $request->input('city');
+            $bank->country_id = $request->input('country_id') ?? (Country::where('admin_id', auth('admin')->user()->id)->first())->id;
+            $bank->admin_id = auth('admin')->user()->id;
+            $isCreated = $bank->save();
+            if ($request->hasFile('sheek_image')) {
+                //abc.png
+                $sheekImageName = time() . '_sheek_images' . '.' . $request->file('sheek_image')->getClientOriginalExtension();
+                $request->file('sheek_image')->storePubliclyAs('public/img/', $sheekImageName);
+                DB::table('images')->insert([
+                    'img' => $sheekImageName,
+                    'bank_id' => $bank->id,
+                ]);
+            }
 
-        if ($isCreated) {
-            // Store Logs
-            $this->storeUserLogs('create bank');
-            session([
-                'created' => true,
-                'title' => 'Bank Created',
-                'message' => 'Bank ' . $request->input('name') . ' created successfully',
-            ]);
-            return redirect()->route('banks.index');
+            if ($isCreated) {
+                // Store Logs
+                $this->storeUserLogs('create bank');
+                session([
+                    'created' => true,
+                    'title' => 'Bank Created',
+                    'message' => 'Bank ' . $request->input('name') . ' created successfully',
+                ]);
+                return redirect()->route('banks.index');
+            } else {
+                session([
+                    'created' => false,
+                    'title' => 'Failed',
+                    'message' => 'Failed to create new bank',
+                ]);
+                return redirect()->route('banks.create');
+            }
+
+            // Create new country from the static system
         } else {
-            session([
-                'created' => true,
-                'title' => 'Failed',
-                'message' => 'Failed to create new bank',
-            ]);
-            return redirect()->route('banks.create');
+
+            $static_country = DB::table('static_countries')->where('id', $request->input('static_country_id'))->first();
+            $adminCountry = Country::where([
+                ['admin_id', auth('admin')->user()->id],
+                ['name', 'LIKE', $static_country->name],
+            ])->first();
+
+            // Check if the admin created this country before or not
+            // Not created before
+            if ($adminCountry) {
+
+                $adminCountry->name = $static_country->name;
+                $adminCountry->admin_id = auth('admin')->user()->id;
+                $isCreatedAdminCountry = $adminCountry->save();
+
+                // Is created ?!
+                if ($isCreatedAdminCountry) {
+                    $bank = new Bank();
+                    $bank->name = $request->input('name');
+                    $bank->city = $request->input('city');
+                    $bank->country_id = $adminCountry->id;
+                    $bank->admin_id = auth('admin')->user()->id;
+                    $isCreated = $bank->save();
+
+                    // If the admin upload an image with bank
+                    if ($request->hasFile('sheek_image')) {
+                        //abc.png
+                        $sheekImageName = time() . '_sheek_images' . '.' . $request->file('sheek_image')->getClientOriginalExtension();
+                        $request->file('sheek_image')->storePubliclyAs('public/img/', $sheekImageName);
+                        DB::table('images')->insert([
+                            'img' => $sheekImageName,
+                            'bank_id' => $bank->id,
+                        ]);
+
+                        // If the admin dosen't upload an image, take the default image from the static bank
+                        // Take defaul image
+                    } else {
+                        $static_bank = DB::table('static_bank')->where('country_id', $static_country->id)
+                            ->orWhere('city', 'LIKE', '%' . $request->input('city') . '%')
+                            ->first();
+
+                        DB::table('images')->insert([
+                            'img' => $static_bank->img,
+                            'bank_id' => $bank->id,
+                        ]);
+                    }
+
+                    if ($isCreated) {
+                        // Store Logs
+                        $this->storeUserLogs('create bank');
+                        session([
+                            'created' => true,
+                            'title' => 'Bank Created',
+                            'message' => 'Bank ' . $request->input('name') . ' created successfully',
+                        ]);
+                        return redirect()->route('banks.index');
+                    } else {
+                        session([
+                            'created' => false,
+                            'title' => 'Failed',
+                            'message' => 'Failed to create new bank',
+                        ]);
+                        return redirect()->route('banks.create');
+                    }
+                }
+            } else {
+                
+                // Start create new country from the system manual
+                $adminCountry = new Country();
+                $adminCountry->name = $static_country->name;
+                $adminCountry->admin_id = auth('admin')->user()->id;
+                $isCreatedAdminCountry = $adminCountry->save();
+
+                // Is created ?!
+                if ($isCreatedAdminCountry) {
+                    $bank = new Bank();
+                    $bank->name = $request->input('name');
+                    $bank->city = $request->input('city');
+                    $bank->country_id = $adminCountry->id;
+                    $bank->admin_id = auth('admin')->user()->id;
+                    $isCreated = $bank->save();
+
+                    // If the admin upload an image with bank
+                    if ($request->hasFile('sheek_image')) {
+                        //abc.png
+                        $sheekImageName = time() . '_sheek_images' . '.' . $request->file('sheek_image')->getClientOriginalExtension();
+                        $request->file('sheek_image')->storePubliclyAs('public/img/', $sheekImageName);
+                        DB::table('images')->insert([
+                            'img' => $sheekImageName,
+                            'bank_id' => $bank->id,
+                        ]);
+
+                        // If the admin dosen't upload an image, take the default image from the static bank
+                        // Take defaul image
+                    } else {
+                        $static_bank = DB::table('static_bank')->where('country_id', $static_country->id)
+                            ->orWhere('city', 'LIKE', '%' . $request->input('city') . '%')
+                            ->first();
+
+                        DB::table('images')->insert([
+                            'img' => $static_bank->img,
+                            'bank_id' => $bank->id,
+                        ]);
+                    }
+
+                    if ($isCreated) {
+                        // Store Logs
+                        $this->storeUserLogs('create bank');
+                        session([
+                            'created' => true,
+                            'title' => 'Bank Created',
+                            'message' => 'Bank ' . $request->input('name') . ' created successfully',
+                        ]);
+                        return redirect()->route('banks.index');
+                    } else {
+                        session([
+                            'created' => false,
+                            'title' => 'Failed',
+                            'message' => 'Failed to create new bank',
+                        ]);
+                        return redirect()->route('banks.create');
+                    }
+                }
+            }
         }
     }
 
