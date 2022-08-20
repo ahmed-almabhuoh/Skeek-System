94e3b21718c3349b971276333ae6333fc8ed147c
Ahmad Almabhoh <az540546@gmail.com>
2022-08-20 16:13:12 +0300

    Some-Edits

--- app/Http/Controllers/Super/Super/Settings/FollowUpSuperController.php
+++ app/Http/Controllers/Super/Super/Settings/FollowUpSuperController.php
@@ -16,7 +16,7 @@ class FollowUpSuperController extends Controller
     public function showSuperUserAction($id)
     {
         $super = Super::findOrFail(Crypt::decrypt($id));
-        $super_logs = DB::table('super_logs')->where('super_id', $super->id)->get();
+        $super_logs = DB::table('super_logs')->where('super_id', $super->id)->latest()->get();
         return response()->view('back-end.supers.supers.follow-up', [
             'super' => $super,
             'super_logs' => $super_logs,
