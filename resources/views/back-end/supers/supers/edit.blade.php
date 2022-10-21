a4755b4f9ec5b66b751aa885881a08f9d63c456b
Ahmad Almabhouh <az54546@gmail.com>
2022-10-21 07:24:40 +0300

    Some-Edits

--- resources/views/back-end/supers/supers/edit.blade.php
+++ resources/views/back-end/supers/supers/edit.blade.php
@@ -14,7 +14,7 @@
         <div class="card-body">
             <div class="alert alert-success alert-dismissible">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                <h5><i class="icon fas fa-check"></i> {{__('Success!')}}</h5>
+                <h5><i class="icon fas fa-check"></i> {{ __('Success!') }}</h5>
                 {{ session('status') }}
             </div>
         </div>
@@ -22,7 +22,7 @@
         <div class="card-body">
             <div class="alert alert-danger alert-dismissible">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                <h5><i class="icon fas fa-ban"></i> {{__('Failed!')}}</h5>
+                <h5><i class="icon fas fa-ban"></i> {{ __('Failed!') }}</h5>
                 {{ session('status') }}
             </div>
         </div>
@@ -30,7 +30,7 @@
     <div class="col-md-12">
         <div class="card card-info">
             <div class="card-header">
-                <h3 class="card-title">{{__('Add New Super')}}</h3>
+                <h3 class="card-title">{{ __('Add New Super') }}</h3>
             </div>
             <!-- /.card-header -->
             <!-- form start -->
@@ -41,14 +41,14 @@
                 <div class="card-body">
                     <div class="form-group row" style="display: none;">
                         <input type="text" name="id" class="form-control" id="id"
-                            value="{{ Crypt::encrypt($super->id) }}" placeholder="{{__('Enter super id')}}">
+                            value="{{ Crypt::encrypt($super->id) }}" placeholder="{{ __('Enter super id') }}">
                     </div>
 
                     <div class="form-group row">
-                        <label for="name" class="col-sm-2 col-form-label">{{__('Full name')}}</label>
+                        <label for="name" class="col-sm-2 col-form-label">{{ __('Full name') }}</label>
                         <div class="col-sm-10">
                             <input type="text" name="name" class="form-control" id="name"
-                                value="{{ $super->name }}" placeholder="{{__('Enter super full name')}}"
+                                value="{{ $super->name }}" placeholder="{{ __('Enter super full name') }}"
                                 style="@error('name') border-color: red; @enderror">
                             @error('name')
                                 <SMAll style="color: red">
@@ -59,11 +59,11 @@
                     </div>
 
                     <div class="form-group row">
-                        <label for="email" class="col-sm-2 col-form-label">{{__('Email')}}</label>
+                        <label for="email" class="col-sm-2 col-form-label">{{ __('Email') }}</label>
                         <div class="col-sm-10">
                             <input type="email" name="email" class="form-control" id="email"
                                 value="{{ $super->email }}" style="@error('email') border-color: red; @enderror"
-                                placeholder="{{__('Enter super email')}}">
+                                placeholder="{{ __('Enter super email') }}">
                             @error('email')
                                 <SMAll style="color: red">
                                     {{ $message }}
@@ -74,12 +74,12 @@
 
 
                     <div class="form-group row">
-                        <label for="password" class="col-sm-2 col-form-label">{{__('Password')}}</label>
+                        <label for="password" class="col-sm-2 col-form-label">{{ __('Password') }}</label>
                         <div class="col-sm-10">
                             <input type="text" name="password" class="form-control" id="password"
-                                style="@error('password') border-color: red; @enderror" placeholder="{{__('Enter super password')}}"
-                                value="{{ $password }}">
-                            <small>{{__('If won\'t change the super password, let it empty.')}}</small>
+                                style="@error('password') border-color: red; @enderror"
+                                placeholder="{{ __('Enter super password') }}" value="{{ $password }}">
+                            <small>{{ __('If won\'t change the super password, let it empty.') }}</small>
                             @error('password')
                                 <SMAll style="color: red">
                                     {{ $message }}
@@ -91,8 +91,9 @@
                     <div class="form-group row">
                         <div class="offset-sm-2 col-sm-10">
                             <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="active" checked>
-                                <label class="form-check-label" for="active">{{__('is Active!')}}</label>
+                                <input type="checkbox" class="form-check-input" id="active" name="active"
+                                    @if ($super->active) checked @endif>
+                                <label class="form-check-label" for="active">{{ __('is Active!') }}</label>
                             </div>
                         </div>
                     </div>
@@ -100,8 +101,8 @@
                 </div>
                 <!-- /.card-body -->
                 <div class="card-footer">
-                    <button type="submit" class="btn btn-info">{{__('Update')}}</button>
-                    <button type="reset" class="btn btn-default float-right">{{__('Cancel')}}</button>
+                    <button type="submit" class="btn btn-info">{{ __('Update') }}</button>
+                    <button type="reset" class="btn btn-default float-right">{{ __('Cancel') }}</button>
                 </div>
                 <!-- /.card-footer -->
             </form>
